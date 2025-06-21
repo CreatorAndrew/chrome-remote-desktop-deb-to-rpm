2de270858327f3b20b146d6cd82cadc87449779f
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2025-06-21 13:30:52 -0500

    Add script

---
+++ chrome-remote-desktop-deb-to-rpm.sh
@@ -0,0 +1,11 @@
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $CURRENT_DIR
+PKG_NAME=$(sudo alien --to-rpm --generate --scripts --fixperms ./chrome-remote-desktop_current_amd64.deb)
+PKG_NAME="${PKG_NAME/Directory /""}"
+PKG_NAME="${PKG_NAME/ prepared./""}"
+cd $PKG_NAME
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sudo sed -i "/^%dir \"\//d" ./$PKG_NAME-2.spec
+rpmbuild --target=x86_64 --buildroot $CURRENT_DIR -bb $PKG_NAME-2.spec > /dev/null
+cd ..
+sudo rm -rf $PKG_NAME
